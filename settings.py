1f7a1ae4ecbb76793ffd1a73fcc267bbbe1a294f
MANOJ KUMAR MOVVA <movvamanoj@gmail.com>
2024-05-04 09:17:38 +0530

    deploy.yml

--- settings.py
+++ settings.py
@@ -1,12 +1,6 @@
-# settings.py
-
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Other settings...
-
-STATIC_URL = '/static/'
-
-# Set the STATIC_ROOT to a directory where you want to collect static files.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Change 'staticfiles' to the desired directory name
+ALLOWED_HOSTS = [
+    os.getenv('EC2_HOST'),
+    f'http://{os.getenv("EC2_HOST")}/'
+]
